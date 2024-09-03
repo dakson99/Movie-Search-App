5f72d37c4a866ce26d165b807216d0ec11192039
dakson99 <darko99.mikavica@gmail.com>
2024-09-03 19:58:11 +0200

    Add basic JavaScript functionality for searching and displaying movies using OMDB API

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 /*https://www.omdbapi.com/?s=avengers&page=1&apikey=77a9305a*/
 /*http://www.omdbapi.com/?i=tt3896198&apikey=77a9305a*/
+
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+// load movies from API
+async function loadMovies(searchTerm) {
+    const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=77a9305a`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    // console.log(data.Search);
+    if (data.Response == "True") displayMovieList(data.Search);
+}
+
+function findMovies() {
+    let searchTerm = (movieSearchBox.value).trim();
+    if (searchTerm.length > 0) {
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+function displayMovieList(movies) {
+    searchList.innerHTML = "";
+    for (let idx = 0; idx < movies.length; idx++) {
+        let movieListItem = document.createElement('div');
+        console.log(movieListItem);
+    }
+}
